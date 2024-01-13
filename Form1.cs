cc02c590f3e12236f9a7eea2a891777b58f755b7
eric <eric@ONGOING>
2024-01-13 11:53:49 +0800

    添加项目文件。

---
+++ Form1.cs
@@ -0,0 +1,24 @@
+using System;
+using MySql.Data.MySqlClient;
+
+
+namespace WinFormsApp2_C_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
